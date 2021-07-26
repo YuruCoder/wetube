7499b6369c4139e0471f335a017968b090d9c699
hojunkim1 <76956067+hojunkim1@users.noreply.github.com>
2021-07-26 16:13:15 +0900

    made pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,6 @@
 // Global Router
 
-export const trending = (req, res) => res.send("Trending Videos");
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
 export const search = (req, res) => res.send("Search Video");
 
 // Video Router
@@ -9,6 +9,10 @@ export const upload = (req, res) => res.send("Upload Videos");
 
 // Video ID Required
 
-export const see = (req, res) => res.send("Watch Video");
-export const edit = (req, res) => res.send("Edit Video");
+export const see = (req, res) =>
+  res.render("watch", { pageTitle: "Watch Video" });
+
+export const edit = (req, res) =>
+  res.render("edit", { pageTitle: "Edit Video" });
+
 export const deleteVideo = (req, res) => res.send("Delete Video");
--- src/server.js
+++ src/server.js
@@ -13,6 +13,7 @@ const logger = morgan("dev");
 
 app.use(logger);
 app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
 
 // App
 
