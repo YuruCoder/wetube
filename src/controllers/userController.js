e94ba5f78797f87445ca2e7b347e8e9d72144a35
hojunkim1 <76956067+hojunkim1@users.noreply.github.com>
2021-08-06 22:37:11 +0900

    made login form

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 // Root Controllers
 
@@ -10,31 +11,61 @@ export const postJoin = async (req, res) => {
   const pageTitle = "Join";
   const { name, email, username, password, password2, location } = req.body;
   // Error Check
+  const exists = await User.exists({ $or: [{ username }, { email }] });
   if (password !== password2) {
     return res.render("join", {
       pageTitle,
       errorMessage: "Password confirmation does not match.",
     });
-  }
-  const exists = await User.exists({ $or: [{ username }, { email }] });
-  if (exists) {
+  } else if (exists) {
     return res.status(400).render("join", {
       pageTitle,
       errorMessage: "This username/email is already taken.",
     });
   }
   // Create Account
-  await User.create({
-    name,
-    email,
-    username,
-    password,
-    location,
-  });
+  try {
+    await User.create({
+      name,
+      email,
+      username,
+      password,
+      location,
+    });
+  } catch (error) {
+    return res.status(400).render("join", {
+      pageTitle: "Join",
+      errorMessage: error._message,
+    });
+  }
   return res.redirect("/login");
 };
 
-export const login = (req, res) => res.send("Log in");
+export const getLogin = (req, res) => {
+  return res.render("login", { pageTitle: "Log in" });
+};
+
+export const postLogin = async (req, res) => {
+  const pageTitle = "Log in";
+  const { username, password } = req.body;
+  // Check Username
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "An account with this username does not exists.",
+    });
+  }
+  // Check Password
+  const ok = await bcrypt.compare(password, user.password);
+  if (!ok) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "Wrong password",
+    });
+  }
+  return res.redirect("/");
+};
 
 // Public User Controllers
 
