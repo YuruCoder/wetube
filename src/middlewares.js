1f7149f2b281c0edf132e092306662b4586768c8
hojunkim1 <76956067+hojunkim1@users.noreply.github.com>
2021-08-09 17:57:28 +0900

    add .env file

--- src/middlewares.js
+++ src/middlewares.js
@@ -2,6 +2,5 @@ export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.loggedInUser = req.session.user;
   res.locals.siteName = "Wetube";
-  console.log(res.locals);
   next();
 };
