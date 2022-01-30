3462797f3330121ec161adb420429c77fb10bd1e
Hojun Kim <76956067+hojunkim1@users.noreply.github.com>
2022-01-30 21:19:05 +0900

    package updates

--- src/db.js
+++ src/db.js
@@ -1,16 +1,12 @@
 import mongoose from "mongoose";
 
-mongoose.connect(process.env.DB_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-});
+const main = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL);
+    console.log("✅ Connected to DB");
+  } catch (error) {
+    console.log("❌ DB Error", error);
+  }
+};
 
-const db = mongoose.connection;
-
-const handleOpen = () => console.log("✅ Connected to DB");
-const handleError = (error) => console.log("❌ DB Error", error);
-
-db.on("error", handleError);
-db.once("open", handleOpen);
+main();
