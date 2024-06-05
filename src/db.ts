d2f41df7fa27cc696554b3ec523d310f301bf688
Hojun Kim <76956067+hojunkim1@users.noreply.github.com>
2024-06-05 18:09:45 +0900

    reconfig

--- src/db.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-try {
-  mongoose.connect(String(process.env.DB_URL));
-  console.log("✅ Connected to DB");
-} catch (error) {
-  console.log("❌ DB Error: ", error);
-}
