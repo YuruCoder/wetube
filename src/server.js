10423c5a02c36ac9df2aacc65d4afce75b5b8d60
Hojun Kim <76956067+hojunkim1@users.noreply.github.com>
2024-06-16 18:42:44 +0900

    express basics

---
+++ src/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import morgan from "morgan";
+
+const PORT = 4000;
+
+const app = express();
+const logger = morgan("dev");
+
+const home = (req, res) => res.send("hello");
+
+app.use(logger);
+app.get("/", home);
+
+const handleListening = () =>
+  console.log(`Server listening on port http://localhost:${PORT} 🚀`);
+
+app.listen(PORT, handleListening);
