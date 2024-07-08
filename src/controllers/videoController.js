b95afc935f353af0fc349b85efb7e1279101d1d3
hojunkim1 <hojunkim02@icloud.com>
2024-07-08 16:21:36 +0900

    database 1

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,43 +1,49 @@
+const videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 3,
+  },
+];
+
 export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "First Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Second Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Third Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-  ];
   res.render("home", { pageTitle: "Home", videos });
 };
 
-export const search = (req, res) => res.send("Search Videos");
-
-export const upload = (req, res) => res.send("Upload Video");
-
-export const see = (req, res) => {
-  res.render("watch");
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
 };
 
-export const edit = (req, res) => {
-  res.render("edit");
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
 };
 
-export const deleteVideo = (req, res) => res.send("Delete Video");
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  videos[id - 1].title = title;
+  res.redirect(`/videos/${id}`);
+};
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,11 +1,10 @@
 import express from "express";
-import { search, trending } from "../controllers/videoController";
+import { trending } from "../controllers/videoController";
 import { join, login } from "../controllers/userController";
 
 const globalRouter = express.Router();
 
 globalRouter.get("/", trending);
-globalRouter.get("/search", search);
 globalRouter.get("/join", join);
 globalRouter.get("/login", login);
 
