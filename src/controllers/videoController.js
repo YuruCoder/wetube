28286ce0673788629928e3f1029385cd4abc507c
Hojun Kim <hojunkim02@icloud.com>
2024-08-20 10:55:49 +0900

    css fin

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,7 +2,9 @@ import User from "../models/User";
 import Video from "../models/Video";
 
 export const home = async (req, res) => {
-  const videos = await Video.find({}).sort({ createdAt: "desc" });
+  const videos = await Video.find({})
+    .sort({ createdAt: "desc" })
+    .populate("owner");
   res.render("home", { pageTitle: "Home", videos });
 };
 
@@ -15,7 +17,7 @@ export const search = async (req, res) => {
       title: {
         $regex: new RegExp(keyword, "i"),
       },
-    });
+    }).populate("owner");
   }
 
   res.render("search", { pageTitle: "Search", videos });
